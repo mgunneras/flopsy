07837399dc730beec9931fc20e46beb9d10745bb
Mattias Gunneras <mattiasgunneras@gmail.com>
2009-07-09 19:26:00 +0100

    Added setup.py file for pip install

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+setup(
+    name='flopsy',
+    description='',
+    long_description='',
+    url='',
+    py_modules=['flopsy'],
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    include_package_data=True,
+    zip_safe=False, 
+)
